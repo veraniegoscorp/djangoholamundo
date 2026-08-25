403b65f600fb8b4a6fce4728c393ea1f60a3d38a
vicente <vicente.valenzuela15@inacapmail.cl>
2026-08-25 13:29:09 -0400

    Add product list view and template
    Add a products listing template (bienvenida/templates/productos/lista.html), a lista_productos view in bienvenida/views.py that queries producto.objects.all() and renders the template, and register the new route 'lista_productos/' in mi_proyecto/urls.py. Also a minor change to the mostrar_bienvenida response text.

--- bienvenida/views.py
+++ bienvenida/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from .models import producto
+from django.shortcuts import render
+
+
 
 def mostrar_bienvenida(request):
-    return HttpResponse("¡Bienvenidos a mi primera app Django, Vicen!")
+    return HttpResponse("oal shavo")
+
+
+def lista_productos(request):
+    productos = producto.objects.all()
+    return render(request, 'productos/lista.html', {'productos':productos})
--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -1,7 +1,11 @@
 
 from django.urls import path
-from bienvenida.views import mostrar_bienvenida
+from bienvenida.views import mostrar_bienvenida,lista_productos
+
+
+
 
 urlpatterns = [
     path('inicio/', mostrar_bienvenida, name='mostrar_bienvenida'),
+    path('lista_productos/', view=lista_productos, name="lista productos")
 ]
